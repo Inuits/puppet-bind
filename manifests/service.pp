7fd33d123bb922f598e6a5faf1dec06f2c2e7169
Pavel Pulec <kayn@inuits.eu>
2015-03-04 18:04:31 +0100

    refs #12958, restart only when config is OK
    Signed-off-by: Pavel Pulec <kayn@inuits.eu>

--- manifests/service.pp
+++ manifests/service.pp
@@ -7,7 +7,7 @@ class bind::service (
 
   if $service_reload {
     Service[$servicename] {
-      restart => "service ${servicename} restart",
+      restart => "/usr/sbin/named-checkconf && service ${servicename} restart",
     }
   }
 
